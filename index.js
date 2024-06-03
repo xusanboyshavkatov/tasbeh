4901bbca8808d65784f8a70554f0efaf777d8a85
xusanboyshavkatov <xusanboyshavkatov0110@gmail.com>
2024-06-03 23:52:03 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let musiqafile = document.querySelector(".musiqafile")
 let musiqaicon = document.querySelector(".musiqaicon")
 function clickbtn() {
     count++
-    window.navigator.vibrate([10])
+    window.navigator.vibrate([50])
     counth1.innerHTML = count
     clickbtntext.innerHTML = count
 }
