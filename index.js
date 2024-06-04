10c0aaaa8719434502cf9c341802e3ce625e315b
xusanboyshavkatov <xusanboyshavkatov0110@gmail.com>
2024-06-04 21:16:47 +0500

    tasbeh.uz versiya3
    dizayn yangilandi  yangi funksiyalar qo'shildi!

--- index.js
+++ index.js
@@ -1,42 +1,82 @@
-let count = 0
-let counth1 = document.querySelector(".counth1")
-let clickbtntext = document.querySelector(".countbtn")
+let tasbeh_popup_input = document.querySelector(".tasbeh_popup_input")
+let count_input = document.querySelector(".count_input")
+let count_input_btninnerhtml = document.querySelector(".count_input_btn")
+let tasbeh_count_h1 = document.querySelector(".tasbeh_count_h1")
 
-
-let musiqafile = document.querySelector(".musiqafile")
 let musiqaicon = document.querySelector(".musiqaicon")
-function clickbtn() {
-    count++
-    window.navigator.vibrate([50])
-    counth1.innerHTML = count
-    clickbtntext.innerHTML = count
-}
-function tasbeh_minus() {
-    count--
-    counth1.innerHTML = count
-    clickbtntext.innerHTML = count
-}
 
-function tasbeh_restart() {
-    count = 0
-    counth1.innerHTML = count
-    clickbtntext.innerHTML = count
-}
+let count = 0
 
-
-isOpen = true
-
+let audio = "";
+let audioopen = 1
+audio = new Audio("audio/click.m4a")
 function musiqa() {
-    if (isOpen) {
-        musiqafile.play()
-        musiqaicon.src = "img/tasbeh_audio_icon.png"
-        isOpen = false
+    if (audioopen === 1) {
+        musiqaicon.src = "img/icons8-vibration-64.png"
+        window.navigator.vibrate([50])
+        audio = ""
+        audioopen++
     }
-    else {
-        musiqafile.pause()
-        isOpen = true
-        musiqaicon.src = "img/tasbeh_noaudio_icon.png"
-        musiqafile.currentTime = 0;
+    else if (audioopen === 2) {
+        musiqaicon.src = "img/icons8-no-audio-50 (2).png"
+        audio = ""
+        audioopen ++
+    }
+    else if (audioopen === 3) {
+        musiqaicon.src = "img/icons8-audio-50 (1).png"
+        audio = new Audio("audio/click.m4a")
+        audioopen = 1
+    }
+}
+
+
+function count_input_btn() {
+    tasbeh_popup_input.classList.remove("display_none")
+}
+
+function save_btn() {
+    count_input_btninnerhtml.innerHTML = count_input.value
+    count_input_btninnerhtml.style.fontSize = "22px";
+    count_input.value = ""
+    tasbeh_popup_input.classList.add("display_none")
+    event.preventDefault();
+}
+function cancel_btn() {
+    tasbeh_popup_input.classList.add("display_none")
+    event.preventDefault();
+}
+
+function tasbeh_count() {
+    let limit = parseInt(count_input_btninnerhtml.innerHTML);
+
+
+    if (isNaN(limit) || count < limit) {
+        count++
+        tasbeh_count_h1.innerHTML = count
+        tasbeh_count_h1.style.fontSize = "54px";
+
+        // Ovoz faylini yuklash
+        // let audio = new Audio("audio/click.m4a");
+        // Ovozni ijro etish
+        audio
+        audio.play();
+    }
+    else if (count < parseInt(count_input_btninnerhtml.innerHTML)) {
+        count++
+        tasbeh_count_h1.innerHTML = count
+        tasbeh_count_h1.style.fontSize = "54px";
     }
 
+
+}
+
+
+function res() {
+    count = 0
+    tasbeh_count_h1.style.fontSize = "24px";
+    tasbeh_count_h1.innerHTML = "Boshlash"
+
+    count_input_btninnerhtml.style.fontSize = "16px";
+    count_input_btninnerhtml.innerHTML = "Maqsadli miqdor!"
+    count_input.value = ""
 }
